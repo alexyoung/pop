f606645f57215cd117e04fd267d9fdb827721433
Alex Young <alex@helicoid.net>
2011-08-02 10:37:26 +0100

    Updated tagsg

--- javascripts/site.js
+++ javascripts/site.js
@@ -2,4 +2,8 @@ $(function() {
   $('.tag-list a.tag').live('click', function() {
     $(this).parents('h2').next('.posts').toggle();
   });
+
+  if (window.location.hash) {
+    $(window.location.hash).next('.posts').show();
+  }
 });
