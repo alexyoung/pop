a2ccc07ad08ef94b4366e51a49996889f8248c36
Alex Young <alex@helicoid.net>
2011-08-10 23:55:22 +0100

    Fixed a bug when sites had dots in the path name

--- test/site_builder.test.js
+++ test/site_builder.test.js
@@ -31,3 +31,26 @@ exports['test isRenderedFile'] = function() {
   assert.ok(siteBuilder.isRenderedFile({ type: 'file jade' }));
   assert.ok(!siteBuilder.isRenderedFile({ type: 'file' }));
 };
+
+exports['test SiteBuilder generates a simple site correctly with a dot in the path'] = function(beforeExit) {
+  var configClone, siteBuilder;
+ 
+  configClone = {
+    permalink: '/:year/:month/:day/:title'
+  , paginate: 20
+  , exclude: ['run\.js', '\.swp']
+  , root: path.join(__dirname, 'fixtures', 'test.site')
+  , output: path.join('/tmp', 'pop-tests', (new Date).getTime().toString() + '-test.site')
+  , autoGenerate: [{'feed': 'feed.xml'}] // Should be ignored with 0 posts
+  , url: 'http://example.com/'
+  , title: 'My Site'
+  };
+ 
+  siteBuilder = pop.generateSite(configClone, false);
+
+  beforeExit(function() {
+    var html = fs.readFileSync(configClone.output + '/index.html', 'utf8');
+    assert.match(html, /Welcome to my site/);
+  });
+};
+
