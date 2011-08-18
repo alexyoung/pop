03aeff0fc33a01bef66cea18a8c8bea3050e60a4
Alex Young <alex@helicoid.net>
2011-08-18 21:14:31 +0100

    Updated site

--- javascripts/site.js
+++ javascripts/site.js
@@ -2,8 +2,4 @@ $(function() {
   $('.tag-list a.tag').live('click', function() {
     $(this).parents('h2').next('.posts').toggle();
   });
-
-  if (window.location.hash) {
-    $(window.location.hash).next('.posts').show();
-  }
 });
