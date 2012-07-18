b102ef41ad5cc9cc5bab7e1105dc3e8d4d0e68ed
Alex Young <alex@helicoid.net>
2012-07-18 17:58:15 +0100

    A test had the wrong notation for the date parameters

--- test/helpers.test.js
+++ test/helpers.test.js
@@ -80,9 +80,9 @@ exports['test hNews'] = function() {
 };
 
 exports['test date helpers'] = function() {
-  var date = (1970, 1, 1, 1, 1);
+  var date = [1970, 0, 1, 1, 1];
   assert.equal(helpers.ds(date), '01 January 1970');
-  assert.equal(helpers.dx(date), '1970-01-01T00:01:00Z');
+  assert.equal(helpers.dx(date), '1970-01-01T01:01:00Z');
 };
 
 exports['test html escape'] = function() {
